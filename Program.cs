b9b2d02c684d66b9c087363ac8d5e1b79e3c47b5
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-18 11:10:02 +0300

    1, 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncSolution
 {
@@ -6,7 +7,20 @@ namespace AsyncSolution
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Divide(1, 0).Wait();
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+
+        public static async Task<int> Divide(int a, int b)
+        {
+            return a / b;
+        } 
     }
 }
