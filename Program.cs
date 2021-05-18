47ca25ebaf198588bf195e0c7b87faadc80aebd0
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-18 13:39:16 +0300

    4

--- Program.cs
+++ Program.cs
@@ -1,54 +1,45 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Concurrent;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AsyncSolution
 {
     class Program
     {
-        
-        // private Int32 m_flag = 0;
-        // private Int32 m_value = 0;
-        private static Boolean s_stopWorker = false;
+        private static ConcurrentStack<int> cs;
+
+        static Program()
+        {
+            cs = new ConcurrentStack<int>();
+        }
         
         static void Main(string[] args)
         {
-            // Ни один из примеров Рихтера по volatile не дал должного эффекта
-            // Также не сработали и сторонние примеры с форумов.
-            // Либо это связано с тем, что оптимизации у моего компилятора не предусмотрена, либо с тем, что
-            // проблемы, илюстрируемые Рихтером были частично устранены. Программа компилируется с х64 jit-ом
-            
-            
-            
-            Console.WriteLine(Environment.Is64BitProcess);
-            
-            Console.WriteLine("Main: letting worker run for 5 seconds");
-            Thread t = new Thread(Worker);
-            t.Start();
-            Thread.Sleep(5000);
-            s_stopWorker = true;
-            Console.WriteLine("Main: waiting for worker to stop");
-            t.Join();
-            
-            // Program p = new Program();
-            // new Thread(p.Thread2).Start();
-            // new Thread(p.Thread1).Start();
+            var t1 = new Thread(Print);
+            var t2 = new Thread(Fill);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
         }
-        
-        private static void Worker(Object o) {
-            Int32 x = 0;
-            while (!s_stopWorker) x++;
-            Console.WriteLine("Worker: stopped when x={0}", x);
+
+        private static void Fill()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                cs.Push(i);
+            }
         }
-        
-        // public void Thread1() {
-        //     m_value = 5;
-        //     m_flag = 1;
-        // }
-        // public void Thread2() {
-        //     if (m_flag == 1)
-        //         Console.WriteLine(m_value);
-        // }
+
+        private static void Print()
+        {
+            for (int i = 0; i < cs.Count; i++)
+            {
+                cs.TryPop(out int res);
+                Console.WriteLine(res);
+            }
+        }
     }
 }
