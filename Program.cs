f0b739e6612e680fc0ba220d15b83e5a083c329e
Alexandr Smirnov <newlinefu@gmail.com>
2021-05-18 13:30:00 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,26 +1,54 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncSolution
 {
     class Program
     {
+        
+        // private Int32 m_flag = 0;
+        // private Int32 m_value = 0;
+        private static Boolean s_stopWorker = false;
+        
         static void Main(string[] args)
         {
-            try
-            {
-                Divide(1, 0).Wait();
-            }
-            catch (AggregateException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            // Ни один из примеров Рихтера по volatile не дал должного эффекта
+            // Также не сработали и сторонние примеры с форумов.
+            // Либо это связано с тем, что оптимизации у моего компилятора не предусмотрена, либо с тем, что
+            // проблемы, илюстрируемые Рихтером были частично устранены. Программа компилируется с х64 jit-ом
+            
+            
+            
+            Console.WriteLine(Environment.Is64BitProcess);
+            
+            Console.WriteLine("Main: letting worker run for 5 seconds");
+            Thread t = new Thread(Worker);
+            t.Start();
+            Thread.Sleep(5000);
+            s_stopWorker = true;
+            Console.WriteLine("Main: waiting for worker to stop");
+            t.Join();
+            
+            // Program p = new Program();
+            // new Thread(p.Thread2).Start();
+            // new Thread(p.Thread1).Start();
         }
-
-
-        public static async Task<int> Divide(int a, int b)
-        {
-            return a / b;
-        } 
+        
+        private static void Worker(Object o) {
+            Int32 x = 0;
+            while (!s_stopWorker) x++;
+            Console.WriteLine("Worker: stopped when x={0}", x);
+        }
+        
+        // public void Thread1() {
+        //     m_value = 5;
+        //     m_flag = 1;
+        // }
+        // public void Thread2() {
+        //     if (m_flag == 1)
+        //         Console.WriteLine(m_value);
+        // }
     }
 }
